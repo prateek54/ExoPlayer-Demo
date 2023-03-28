003db130f2f45a611b65d158cfc4e3a4251bbd7d
prateek.batra <prateek.batra@gojek.com>
2023-03-28 16:40:07 +0530

    Update Exoplayer Version

--- app/src/main/java/com/prateek/exoplayerdemo/MainActivity.kt
+++ app/src/main/java/com/prateek/exoplayerdemo/MainActivity.kt
@@ -2,16 +2,16 @@ package com.prateek.exoplayerdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), Player.EventListener {
-    private var player: SimpleExoPlayer? = null
+class MainActivity : AppCompatActivity(), Player.Listener {
+    private var player: ExoPlayer? = null
     private var playbackPosition = 0L
     private var playWhenReady = true
 
@@ -21,13 +21,12 @@ class MainActivity : AppCompatActivity(), Player.EventListener {
     }
 
     private fun initPlayer() {
-        player = SimpleExoPlayer.Builder(this).build()
+        player = ExoPlayer.Builder(this).build()
         player?.playWhenReady = true
         player_exo.player = player
-        val defaultHttpDataSourceFactory = DefaultHttpDataSourceFactory("ExoPlayerDemoUserAgent")
         val mediaItem = MediaItem.fromUri("https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")
         val mediaSource =
-            HlsMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
+            HlsMediaSource.Factory( DefaultHttpDataSource.Factory()).createMediaSource(mediaItem)
         player?.setMediaSource(mediaSource)
         player?.seekTo(playbackPosition)
         player?.playWhenReady = playWhenReady
